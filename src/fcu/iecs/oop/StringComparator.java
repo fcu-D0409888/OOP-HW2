f0c50dc28a58943a522f8c8ba3022a441716fd5b
user <user@FF08>
2016-10-07 09:41:21 +0800

    Exercise 3

---
+++ src/fcu/iecs/oop/StringComparator.java
@@ -0,0 +1,25 @@
+package fcu.iecs.oop;
+
+import java.util.Scanner;
+
+public class StringComparator{
+
+	public static void main(String[] args) {
+
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter a string1 : ");
+		String string1 = keyboard.next();
+		System.out.println("Enter a string2 : ");
+		String string2 = keyboard.next();
+		
+		
+		
+		if(string1.equalsIgnoreCase(string2)){
+			System.out.println("The two strings are the same.");
+		}
+		else{
+			System.out.println("The two strings are not the same.");
+		}
+	}
+}
