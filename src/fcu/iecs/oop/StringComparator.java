09c1344eeeaa2e1ebe356367023d4df28a68ad1a
user <user@FE08>
2016-10-14 09:56:26 +0800

    10/14 Exercise3

--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -8,18 +8,27 @@ public class StringComparator{
 
 		Scanner keyboard = new Scanner(System.in);
 		
-		System.out.println("Enter a string1 : ");
-		String string1 = keyboard.next();
-		System.out.println("Enter a string2 : ");
-		String string2 = keyboard.next();
+		int num = 1;
 		
-		
-		
-		if(string1.equalsIgnoreCase(string2)){
-			System.out.println("The two strings are the same.");
+		while(num==1)
+		{
+			System.out.println("Enter a string1 : ");
+			String string1 = keyboard.next();
+			System.out.println("Enter a string2 : ");
+			String string2 = keyboard.next();
+
+			if(string1.equalsIgnoreCase(string2)){
+				System.out.println("The two strings are the same.");
+				break;
+			}
+			else{
+				System.out.println("The two strings are not the same.");
+			}
 		}
-		else{
-			System.out.println("The two strings are not the same.");
-		}
+		
+		
+		
+		
+		keyboard.close();
 	}
 }
